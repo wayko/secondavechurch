b2f9725dd0aaf15c39620883a457ecbf97b7d873
Wayko <programmers@dreamcpu.com>
2018-02-02 13:06:41 -0500

    Got calendar summary
    Got calendar summary
    Formated and displayed items per line

--- calendar2.php
+++ calendar2.php
@@ -20,6 +20,7 @@
   
   // Allow access to Google API when the user is not present. 
   $client->setAccessType('offline');
+  $client->setApprovalPrompt ("force");
   $client->setRedirectUri($REDIRECT_URI);
   $client->setScopes($SCOPES);
   
@@ -79,15 +80,126 @@ $optParams = array(
  $results = $service->events->listEvents($calendarId, $optParams);
 
 if (count($results->getItems()) == 0) {
-  print "No upcoming events found.\n";
+  echo "No upcoming events found.\n";
 } else {
-  print "Upcoming events:\n";
+  echo "Upcoming events:\n";
   foreach ($results->getItems() as $event) {
     $start = $event->start->dateTime;
+	$description = $event->getDescription();
+	$formattedDate = date_format(new DateTime($start),"F j, Y - G:i A");
+	if (empty($description)){
+		$description = "No Description";
+	}
     if (empty($start)) {
       $start = $event->start->date;
     }
-    printf("%s (%s)\n", $event->getSummary(), $start);
+	
+   echo "<div class='calendar'><h1>" .$event->getSummary() . "</h1>  <h3 class='getdates'>" . $formattedDate . " </h3><br /><span class='description'>" . $description . "</span></div>";
   }
 }
 ?>
+<!DOCTYPE html>
+<html ng-app="SACNYC">
+<head>
+<title>Second Avenue Church</title>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script>
+<script src="js/ngCart/dist/ngCart.js" type="text/javascript"></script>
+<script src="js/ui-bootstrap-tpls.min.js" type="text/javascript"></script>
+<script src="js/app.js" type="text/javascript"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/angular-translate/2.7.2/angular-translate.js" type="text/javascript"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.15/angular-sanitize.js" type="text/javascript"></script>
+
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
+<link href="http://dreamcpu.com/css/skel-noscript.css" rel="stylesheet">
+<link href="css/style-desktop.css" rel="stylesheet">
+<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:300,600,700">
+<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Tangerine">
+<link href="http://dreamcpu.com/css/font-awesome.css" rel="stylesheet">
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.1/css/font-awesome.css">
+<link href="css/style2.css" rel="stylesheet">
+</head>
+<body>
+<!--Navigation Section-->
+<section>
+<nav id="nav" class="navbar navbar-default colorpick fullwidth">
+<div class="container-fluid navwrap">
+<div class="row">
+<div class="navbar-header">
+<div class="navbar-collapse centernav" id="bs-example-navbar-collapse-1 ">
+
+   <ul class="nav navbar-nav">
+<li class="dropdown">
+    <a class="url" type="button" data-toggle="dropdown">Visit</a>
+    <ul class="dropdown-menu">
+      <li><a href="Directions.html">Directions</a></li>
+      <li><a href="#">Services</a></li>
+	  </ul>
+	  </li>
+    </ul>
+
+
+   <ul class="nav navbar-nav">
+<li class="dropdown">
+    <a class="url" type="button" data-toggle="dropdown">About</a>
+    <ul class="dropdown-menu">
+      <li><a href="#">History of the church</a></li>
+      <li><a href="#">What we believe</a></li>
+      <li><a href="#">Historical Landmark date</a></li>
+	  <li><a href="#">Renovation project in progress</a></li>
+	  <li><a href="http://www.cmalliance.org">CMA</a></li>
+	  <li><a href="#">Historical Landmark date</a></li>
+	  </ul>
+	  </li>
+    </ul>
+
+<ul class="nav navbar-nav">
+<li class="dropdown">
+    <a class="url" type="button" data-toggle="dropdown">Ministries</a>
+    <ul class="dropdown-menu">
+      <li><a href="#">Women's Ministries</a></li>
+      <li><a href="#">Men's Ministries</a></li>
+      <li><a href="#">Young Adult Ministries</a></li>
+	  <li><a href="#">Youth Ministries</a></li>
+	  <li><a href="#">Kid's Ministries</a></li>
+	  </ul>
+    </li>
+  </ul>
+
+
+<ul class="nav navbar-nav">
+<li class="dropdown">
+    <a href="donations.html" class="url">Give</a>
+  </li>
+  </ul>
+
+</div>
+</div>
+</div>
+</div>
+</nav>
+</section>
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.7.min.js"></script>
+	<script src="js/bootstrap.js" type="text/javascript"></script>
+<script src="js/sacontact.js" type="text/javascript"></script>
+<script src="js/menuHover.js" type="text/javascript"></script>
+<script type="text/javascript">
+$(document).ready(function(){
+function formatNumber(newNumb){
+	var numArray = []
+	numArray.join(newNumb);
+return newNumb;	
+}
+var endval
+	$(".amount_number").blur(function(){
+	var newval = $(".amount_number").val();
+	 $(".amount_number").val(formatNumber(newval) + '.00');
+	});
+	
+});
+</script>
+</body>
+
+<!--End Contact Us Section-->
+
+</html>
--- js/menuHover.js
+++ js/menuHover.js
@@ -6,6 +6,40 @@ $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
 $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
 });
 
+var showChar = 45;  // How many characters are shown by default
+    var ellipsestext = "...";
+    var moretext = "More >";
+    var lesstext = "< Less";
+    
+
+    $('.description').each(function() {
+        var content = $(this).html();
+		
+        if(content.length > showChar) {
+ 
+            var c = content.substr(0, showChar);
+            var h = content.substr(showChar, content.length - showChar);
+ 
+            var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+ 
+            $(this).html(html);
+        }
+ 
+    });
+ 
+    $(".morelink").click(function(){
+        if($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
+
 
 
 });
